e59f9a1e08d5f1669254af1f15bfac4accbd1cc8
chrxtina <christina.ad.nguyen@gmail.com>
2016-08-21 12:36:19 -0400

    Finished Game Logic w/ Arrays

--- christinaMemoryGame/main.js
+++ christinaMemoryGame/main.js
@@ -1,27 +1,44 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-/* if (cardOne === cardTwo){
-	alert("You found a match!");
-} 
-else if (cardThree === cardFour){
-	alert("You found a match!");
-} 
-else {
-	alert("Sorry, try again.")
-}
-*/
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
-
 var createBoard = function(){
-	for (var i = 0; i < 4; i++){
-	var cardElement = document.createElement('div');
-	cardElement.className = "card";
-	board.appendChild(cardElement);
-	};
-};
+  for (var i=0; i<cards.length; i++) {
+      var cardElement = document.createElement('div');
+      cardElement.className = 'card';
+      cardElement.setAttribute('data-card', cards[i]);
+      cardElement.addEventListener('click', isTwoCards);
+
+      board.appendChild(cardElement);
+    }
+}
+var isTwoCards = function(){
+  cardsInPlay.push(this.getAttribute('data-card'));
+  
+  console.log(this.getAttribute('data-card'));
+    if (this.getAttribute('data-card') === 'king') {
+      this.innerHTML = "<img src='king.png' alt='King of Hearts'>"; 
+    } else {
+      this.innerHTML = "<img src='queen.png' alt='Queen of Hearts'>";
+    }
+    if (cardsInPlay.length === 2) {
+        isMatch(cardsInPlay);
+        cardsInPlay = [];
+      }
+}
+var isMatch = function(){
+  setTimeout(function(){
+
+    if (cardsInPlay[0] === cardsInPlay[1]){
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+
+    for (i = 0; i<cards.length; i++){
+    document.getElementsByClassName('card')[i].innerHTML = " ";
+    }
+  }, 500);
+}
 
 createBoard();
